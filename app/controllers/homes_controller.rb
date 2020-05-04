649bb493272f7e5125c179952439b42e0442dcfd
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-04 04:33:06 +0000

    location complete

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @tourist_spots_rank = TouristSpot.find(Favorite.group(:tourist_spot_id).order('count(tourist_spot_id) desc').limit(3).pluck(:tourist_spot_id))
+    gon.tourist_spots = TouristSpot.all
     @genres = Genre.all
     @scenes = Scene.all
   end
