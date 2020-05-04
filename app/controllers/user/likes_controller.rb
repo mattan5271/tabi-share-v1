01a8a93161bdcc3aff4afe462bce52deb4d129f1
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-04 08:03:44 +0000

    use_rank complete

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -3,13 +3,15 @@ class User::LikesController < ApplicationController
     review = Review.find(params[:review_id])
     like = current_user.likes.new(review_id: review.id)
     like.save
-    review.create_notification_like!(current_user)
+    review.create_notification_like!(current_user) #いいねしたことを通知
+    like.user_rank_update(review) #いいねをしたレビューを投稿したユーザーのランク管理
     redirect_to user_tourist_spot_reviews_path
   end
   def destroy
     review = Review.find(params[:review_id])
     like = current_user.likes.find_by(review_id: review.id)
     like.destroy
+    like.user_rank_update(review) #いいねを外したレビューを投稿したユーザーのランク管理
     redirect_to user_tourist_spot_reviews_path
   end
 end
