2cb6ca119960945ca68d9a40038b0d44cccc3ea6
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-14 06:37:48 +0000

    deploy-test

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,15 +28,15 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
-  # def reject_user
-  #   user = User.find_by(email: params[:user][:email].downcase)
-  #   if user
-  #     if (user.valid_password?(params[:user][:password]) && (user.active_for_authentication? == true))
-  #       flash[:error] = "退会済みです。"
-  #       redirect_to new_user_session_path
-  #     end
-  #   else
-  #     flash[:error] = "必須項目を入力してください。"
-  #   end
-  # end
+  def reject_user
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user
+      if (user.valid_password?(params[:user][:password]) && (user.active_for_authentication? == true))
+        flash[:error] = "退会済みです。"
+        redirect_to new_user_session_path
+      end
+    else
+      flash[:error] = "必須項目を入力してください。"
+    end
+  end
 end
