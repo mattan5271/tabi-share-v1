64723c774c40e70b56419b5e56a1ce1dbaf7f283
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-16 10:03:08 +0000

    deploy_image_test

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:matsubishi5/TaBi_Share.git"
 set :deploy_to, "/home/ec2-user/TaBi_Share"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
