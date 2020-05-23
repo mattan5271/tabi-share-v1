1a4da17f3e435119386d093495437b44878c9b6f
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 04:11:39 +0000

    deploy_cron

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,11 +61,12 @@ Rails.application.configure do
 
   # default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => 'smtp.gmail.com',
     :port => 587,
     :user_name => ENV['GMAIL_ADDRESS'],
     :password => ENV['GMAIL_PASSWORD'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,17 +96,15 @@ Rails.application.configure do
 
   # default url
   config.action_mailer.default_url_options = { host: 'tabi-share.work' }
-  # mail setting
-  mail = ENV['GMAIL_ADDRESS']
-  pass = ENV['GMAIL_PASSWORD']
 
+  # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => mail,
-    :password => pass,
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
