ef7c8395badd0040dfb04b9b6f5d2e6fabad312d
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 07:44:42 +0000

    S3-test-4

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,6 +20,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(png jpg)
+  end
+
+  def filename
+    original_filename if original_filename
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
