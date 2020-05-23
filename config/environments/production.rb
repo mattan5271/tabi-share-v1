960486feba3419915e018a73c50870ffa095e537
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 01:44:18 +0000

    fix setting_mail2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,9 @@ Rails.application.configure do
 
   config.serve_static_assets = true # Carrierwaveが表示されない問題
 
+  # default url
+  config.action_mailer.default_url_options = { host: 'tabi-share.work' }
   # mail setting
-  host = 'tabi-share.work'
-  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
