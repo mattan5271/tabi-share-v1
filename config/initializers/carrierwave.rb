c5f41ac0459af6a750086b08fe9c16be36204095
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 12:22:03 +0000

    [hotfix]S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,8 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'tabi-share'
+    config.asset_host = 'https://s3.amazonaws.com/tabi-share'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
