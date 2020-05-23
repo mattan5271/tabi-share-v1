9d0133d0958ed46984cf0300f52827db23e67031
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 05:51:19 +0000

    complete cron_production2

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,7 @@
 
 env :PATH, ENV['PATH'] # 絶対パスから相対パス指定
 set :output, 'log/cron.log' # ログの出力先ファイルを設定
-# set :environment, :development # 環境を設定
+set :environment, :development # 環境を設定
 
 
 every 1.minute do
