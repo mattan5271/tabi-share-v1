7c05293ed880e198f096ae0a4cc28f363254d730
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 09:26:57 +0000

    S3-test-7

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,6 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file
   else
     storage :fog
+    config.fog_provider = 'fog/aws'
   end
 
   # Override the directory where uploaded files will be stored.
