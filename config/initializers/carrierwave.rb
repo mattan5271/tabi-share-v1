ba8cc0a6deebf71684af7cf86fabdf4fb207c1ea
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 11:31:58 +0000

    S3-test-18

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,23 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    # config.storage :fog
-    # config.fog_provider = 'fog/aws'
-    # config.fog_directory  = 'tabi-share'
-    # config.fog_authenticated_url_expiration = 60
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tabi-share'
-    config.fog_public = false
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'tabi-share'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
-
-    config.fog_directory  = 'tabi-share'
-    config.cache_storage = :fog
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
