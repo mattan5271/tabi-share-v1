4feb88715dffdfdf64eb531801f4974fee395caf
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-23 10:54:21 +0000

    S3-test-15

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  # config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -92,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets = true # Carrierwaveが表示されない問題
+  # config.serve_static_assets = true # Carrierwaveが表示されない問題
 
   # default url
   config.action_mailer.default_url_options = { host: 'tabi-share.work' }
