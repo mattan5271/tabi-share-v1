604bf22fde9bcc63ef682440e9cb3a9b53d5b012
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-05-27 02:08:07 +0000

    [fix]impressionist

--- db/migrate/20200527020506_add_impressionist_count_to_tourist_spot.rb
+++
@@ -1,5 +0,0 @@
-class AddImpressionistCountToTouristSpot < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tourist_spots, :impressionist_count, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_020506) do
+ActiveRecord::Schema.define(version: 2020_05_26_014409) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -216,7 +216,6 @@ ActiveRecord::Schema.define(version: 2020_05_27_020506) do
     t.integer "row_order"
     t.text "is_parking", null: false
     t.integer "impressions_count"
-    t.integer "impressionist_count"
   end
 
   create_table "users", force: :cascade do |t|
