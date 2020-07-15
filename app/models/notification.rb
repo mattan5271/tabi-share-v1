8341bfd15b280668b25386d1936ffd916ba3772e
matsubishi5 <matsubishi5@yahoo.co.jp>
2020-07-15 04:15:37 +0000

    [fix]notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,4 @@
 class Notification < ApplicationRecord
-  default_scope -> { order(created_at: :desc) }
   belongs_to :review, optional: true
   belongs_to :comment, optional: true
   belongs_to :message, optional: true
